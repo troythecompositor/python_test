9577aa1e2b855806be516e07a0a620c24133dc77
troythecompositor <87882065+troythecompositor@users.noreply.github.com>
2025-02-22 11:40:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test passed')
+        print('Pass')
     else:
-        print('Test failed')
+        print('Fail')
 
 in_autotests_we_trust(10, '10')
 
